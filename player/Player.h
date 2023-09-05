9768202af4f9c2ee6c8cf8fb57c08dba76e6b968
チャン スギ <junjunchan64@gmail.com>
2023-09-05 21:59:19 +0900

    playerの動きを仕様書に基づいて(イージングとスムージングはまだ)

--- player/Player.h
+++ player/Player.h
@@ -8,9 +8,18 @@ public:
 	void Draw();
 
 private:
-	Vector2 pos_;//顏の位置
-	Vector2 mouseWay_;//マウスの位置
+	static const int MAX_BODY = 255; //体の最大数
+	static const int NUM_NECK = 5;	 //首から上の数
+	static const int SPEED_NECK = 5; //首の最大回転スピード
+
+	Vector2 pos_[MAX_BODY];//顏の位置
+	Vector2 neckWay_;//首の角度
+	int oldNeckWay_;//1フレ前の角度
 	Vector2 originPos_;//チンアナゴの出る位置
-	int length_;
 	int mouseX_, mouseY_;
+
+	int maxLength_;
+	int activeLength_;
+
+	int mouseAngle_;//マウスの角度
 };
