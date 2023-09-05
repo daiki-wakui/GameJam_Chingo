218f2b72698db85026b09105dcfcb59f7b09adf7
チャン スギ <junjunchan64@gmail.com>
2023-09-06 00:10:44 +0900

    チンアナゴのデバッグ表示

--- player/Player.cpp
+++ player/Player.cpp
@@ -23,6 +23,8 @@ void Player::Update()
 	if (DebugManager::GetInstance()->GetIsDebugMode()) {
 		maxLength_ = MAX_BODY - 1;
 	}
+	DrawFormatString(0,60,GetColor(255,255,255),"bodyMaxLength = %d",maxLength_);
+	DrawFormatString(0, 80, GetColor(255, 255, 255), "nowLength = %d", activeLength_);
 
 	//ƒ}ƒEƒX‚ÌêŠæ“¾
 	GetMousePoint(&mouseX_, &mouseY_);
