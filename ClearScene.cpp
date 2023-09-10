83f0e9a3b64b7adf7ef2573293ab50264f682539
daiki-wakui <k021g1119@g.neec.ac.jp>
2023-09-11 03:35:35 +0900

    背景とできたちんあなごを画像で描画

--- ClearScene.cpp
+++ ClearScene.cpp
@@ -12,6 +12,13 @@ void ClearScene::Update()
 
 void ClearScene::Draw()
 {
+	DrawGraph(0, -180, backBottomImage, true);
+
+	DrawGraph(0, 0, blueImage, true);
+	DrawGraph(0, 0, vignetImage, true);
+
+	DrawRotaGraph3(620, 450,
+		256, 256, 1.5, 1.5, 0, chinanaImage, true);
 	ExBodyManager::GetInstance()->ResultDraw();
 
 	DrawFormatString(0, 0, GetColor(255, 255, 255), "clear");
